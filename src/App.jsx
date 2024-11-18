fa289c14142445af59d21e3497dd2e6b94a2aad4
Chetan Yadav <chetanyadav372002@gmail.com>
2024-11-18 13:51:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./component/navigation";
+import Hero from "./component/hero"; // Note: component names should be capitalized
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/hero.jsx
@@ -0,0 +1,26 @@
+const Hero = () => {
+    return (
+      <div className="main">
+        <div className="con">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <div>
+            YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES. 
+            YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+          </div>
+          <button>Shop Now</button>
+          <button>Category</button>
+          <div className="av">Also Available on</div>
+          <div className="sho">
+            <img src="images/123.png" alt="shop logo 1" className="shop" />
+            <img src="images/12.png" alt="shop logo 2" className="shop" />
+          </div>
+        </div>
+        <div className="heroimage">
+          <img src="images/th.jpg" alt="hero banner" className="imag" />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Hero;
+  
